7dcafe9bc99c4dfe04e365082638f77e0de2c4f4
Yago LM <yagolopes777@hotmail.com>
2016-11-30 16:37:47 -0400

    JS1

--- JS1/funcoes.js
+++ JS1/funcoes.js
@@ -4,12 +4,11 @@
 
 for(var i=1; i<=10; i++){
 
-    document.writeln("<table border='1'><tr colspan='2'><td>Produto "+i+"</td></tr>");
-
+    document.writeln("<table border='1'><tr><td colspan='2'><b>Produtos de "+i+"</b></td>");
 
             for(var j=1; j<=10; j++){
-                document.writeln("<td > "+i+" * "+j+" </td><td>"+i * j+"</td>");
+                document.writeln("<tr><td > "+i+" * "+j+" </td><td>"+i * j+"</td></tr>");
             }
 
-    document.writeln("</table>");
+    document.writeln("</tr></table>");
 }
