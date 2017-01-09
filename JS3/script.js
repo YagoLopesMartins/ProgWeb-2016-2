b901c4d4c456f465fe7234feecc63dbb99eecb13
Yago LM <yagolopes777@hotmail.com>
2017-01-09 17:42:21 -0400

    PHP1

---
+++ JS3/script.js
@@ -0,0 +1,5 @@
+/**
+ * Created by yago on 04/01/17.
+ */
+
+
---
+++ PHP1/processa.php
@@ -0,0 +1,13 @@
+<?php
+    $nome = $_GET['nome'];
+    $email = $_GET['email'];
+    $site = $_GET['site'];
+    $mensagem = $_GET['mensagem'];
+    
+    echo "Nome: ".$nome."</br>";
+    echo "Email: ".$email."</br>";
+    echo "WebSite: ".$site."</br>";
+    
+    echo "Mensagem: ".$mensagem."</br>";
+?>
+
